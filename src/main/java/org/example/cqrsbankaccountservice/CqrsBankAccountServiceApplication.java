a2d7d89b1feee7146e327c4c9bdccedc763153d9
SouhailOUARGUI <ouargui550@gmail.com>
2025-12-29 02:11:47 +0100

    Initial commit

---
+++ src/main/java/org/example/cqrsbankaccountservice/CqrsBankAccountServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.cqrsbankaccountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CqrsBankAccountServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CqrsBankAccountServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/cqrsbankaccountservice/CqrsBankAccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cqrsbankaccountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CqrsBankAccountServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
